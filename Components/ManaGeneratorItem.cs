f16075b2bb34f140e69f719bb7804cb0d4599f73
TacoConKvass <e_frun@o2.pl>
2024-07-13 23:40:52 +0300

    [Addition] - ManaGeneratorItem component

---
+++ Components/ManaGeneratorItem.cs
@@ -0,0 +1,20 @@
+using ManaOverhaul.DataStructures;
+using Terraria;
+
+public class ManaGeneratorItem : ItemComponent {
+	public struct ManaGenerationData {
+	 	public Range Variation { get; set; } = new();
+	 	public int BaseManaGeneration { get; set }
+	 	public int ManaGeneration => BaseManaGeneration * (Variation.Upper == Variation.Lower ? 1f : Variation.Random);
+	 }
+	 
+	 public ManaGenerationData Data = new
+	 
+	 public override void OnHitNPC(Item item, Player player, NPC target, NPC.HitInfo hit, int damageDone) {
+	 	if (!Enabled) {
+	 		return;
+	 	}
+	 	
+	 	player.healMana(Data.ManaGeneration);
+	 }
+}
