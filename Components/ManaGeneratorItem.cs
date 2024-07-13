066d8863fc739afc51c2fd378578f2ff9c52994e
TacoConKvass <e_frun@o2.pl>
2024-07-13 23:44:36 +0300

    [Fix] - `Data = new` => `Data = new()`

--- Components/ManaGeneratorItem.cs
+++ Components/ManaGeneratorItem.cs
@@ -5,18 +5,18 @@ namespace ManaOverhaul.Components;
 
 public class ManaGeneratorItem : ItemComponent {
 	public struct ManaGenerationData {
-	 	public Range Variation { get; set; } = new();
-	 	public int BaseManaGeneration { get; set }
-	 	public int ManaGeneration => BaseManaGeneration * (Variation.Upper == Variation.Lower ? 1f : Variation.Random);
-	 }
+		public Range Variation { get; set; } = new();
+		public int BaseManaGeneration { get; set }
+		public int ManaGeneration => BaseManaGeneration * (Variation.Upper == Variation.Lower ? 1f : Variation.Random);
+	}
 	 
-	 public ManaGenerationData Data = new
+	public ManaGenerationData Data = new();
 	 
-	 public override void OnHitNPC(Item item, Player player, NPC target, NPC.HitInfo hit, int damageDone) {
-	 	if (!Enabled) {
+	public override void OnHitNPC(Item item, Player player, NPC target, NPC.HitInfo hit, int damageDone) {
+		if (!Enabled) {
 	 		return;
 	 	}
 	 	
 	 	player.healMana(Data.ManaGeneration);
-	 }
+	}
 }
