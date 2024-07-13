f4b705f4350ce104adcc4750a1191d990d4e7a5c
TacoConKvass <e_frun@o2.pl>
2024-07-13 12:28:38 +0300

    [Addition] - Remove natural mana regen and prevent mana healing from items

---
+++ Common/GlobalManaPickup.cs
@@ -0,0 +1,11 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ManaOverhaul.Common;
+
+internal class GlobalManaPickup : GlobalItem {
+	public override bool AppliesToEntity(Item entity, bool lateInstantiation) {
+		return lateInstantiation && ItemID.Sets.IsAPickup[entity.type] && entity.healMana != 0;
+	}
+}
