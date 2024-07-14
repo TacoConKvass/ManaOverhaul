6a5bed18a3a2ca512fbeeb0b2c06c81fb3cc1cc1
TacoConKvass <e_frun@o2.pl>
2024-07-14 13:50:52 +0300

    [Addition] - ChangeScaleWithManaData documentation

--- Components/ChangeScaleWithManaItem.cs
+++ Components/ChangeScaleWithManaItem.cs
@@ -3,13 +3,26 @@ using Terraria;
 namespace ManaOverhaul.Components;
 
 public class ChangeScaleWithManaItem : ItemComponent {
+	/// <summary
+	/// Data for changing scale on mana thresholds
+	/// </summary>
 	public record struct ChangeScaleWithManaData {
 		public ChangeScaleWithManaData() { }
 		
+		/// <summary
+		/// Thresholds for changing item scale
+		/// </summary>
 		public float[] Thresholds { get; set; } = [0f];
+		/// <summary
+		/// Items scale boosts activated at thresholds at thier index
+		/// </summary>
+		/// <remarks>
+		/// The boosts are additive with eachother
+		/// </remarks>
 		public float[] ScaleBoosts { get; set; } = [0f];
 	}
 	
+	/// <inheritdoc cref="ChangeScaleWithManaData"/>
 	ChangeScaleWithManaData Data = new();
 	
 	public override ModifyItemScale(Item item, Player player, ref float scale) {
