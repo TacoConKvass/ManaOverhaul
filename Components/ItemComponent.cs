ea161d79ee213cd4a6a12f2c2770a8160cfe5558
TacoConKvass <e_frun@o2.pl>
2024-07-14 11:27:19 +0300

    [Addition] - Enabled documentation

--- Components/ItemComponent.cs
+++ Components/ItemComponent.cs
@@ -4,6 +4,9 @@ namespace ManaOverhaul.Components;
 
 public class ItemComponent : GlobalItem {
 	public override bool InstancePerEntity { get; } = true;
-	
+
+	/// <summary
+	/// Determines whether this component is enabled
+	/// </summary>
 	public bool Enabled { get; set; } = false;
 }
--- Components/ProjectileComponent.cs
+++ Components/ProjectileComponent.cs
@@ -5,5 +5,8 @@ namespace ManaOverhaul.Components;
 public class ProjectileComponent : GlobalProjectile {
 	public override bool InstancePerEntity { get; } = true;
 	
+	/// <summary
+	/// Determines whether this component is enabled
+	/// </summary>
 	public bool Enabled { get; set; } = false;
 }
