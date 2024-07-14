bc6c26352184751e3254599e37a49c0200f45036
TacoConKvass <e_frun@o2.pl>
2024-07-14 11:37:18 +0300

    [Addition] - OnHitManaGenerator(Item|Projectile) documentation

--- Components/OnHitManaGeneratorItem.cs
+++ Components/OnHitManaGeneratorItem.cs
@@ -4,14 +4,27 @@ using Terraria;
 namespace ManaOverhaul.Components;
 
 public class OnHitManaGeneratorItem : ItemComponent {
+	/// <summary
+	/// Data for on hit mana regeneration
+	/// </summary>
 	public record struct OnHitManaGenerationData {
 		public OnHitManaGenerationData() { }
 		
+		/// <summary
+		/// Determines the variation range of amount of mana regenerated
+		/// </summary>
 		public Range Variation { get; set; } = new();
+		/// <summary
+		/// Base amount of mana regenerated
+		/// </summary>
 		public int BaseManaGeneration { get; set } = 0;
+		/// <summary
+		/// The amount of mana the hot will actually generate
+		/// </summary>
 		public int ManaGeneration => BaseManaGeneration * (Variation.Upper == Variation.Lower ? 1f : Variation.Random);
 	}
 	 
+	/// <inheritdoc cref="OnHitManaGenerationData"/>
 	public OnHitManaGenerationData Data = new();
 	 
 	public override void OnHitNPC(Item item, Player player, NPC target, NPC.HitInfo hit, int damageDone) {
