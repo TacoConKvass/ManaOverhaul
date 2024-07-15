17aa7e347d84a966706d9ad1fd0ee78eac17045f
TacoConKvass <e_frun@o2.pl>
2024-07-15 22:12:51 +0300

    [Addition] - Attach ChangeScaleWithManaItem component to melee weapons

--- Common/GlobalTrueMeleeWeapon.cs
+++ Common/GlobalTrueMeleeWeapon.cs
@@ -1,4 +1,5 @@
 using ManaOverhaul.Components;
+using ManaOverhaul.DataStructures;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -6,5 +7,14 @@ namespace ManaOverhaul.Common;
 
 public class GlobalTrueMeleeWeapon : GlobalItem {
 	public override bool AppliesToEntity(Item entity, bool lateInstantiation) 
-		=> lateInstantiation && entity.CountsAsClass(DamageClass.Melee) && !entity.noMelee;
+		=> lateInstantiation && entity.CountsAsClass(DamageClass.Melee) && !entity.noMelee && (entity.ModItem == null || entity.ModItem?.Mod != Mod);
+
+	public override void SetDefaults(Item entity) {
+		ChangeScaleWithManaItem changeScaleGlobal = entity.GetGlobalItem<ChangeScaleWithManaItem>();
+		changeScaleGlobal.Enabled = true;
+		changeScaleGlobal.Data = ComponentDataLibrary.ChangeScaleWithManaItem.TryGetValue(entity.type, out ChangeScaleWithManaItem.ComponentData value) ? value : new ChangeScaleWithManaItem.ComponentData() {
+			Thresholds = [.5f],
+			ScaleBoosts = [.3f]
+		};
+	}
 }
